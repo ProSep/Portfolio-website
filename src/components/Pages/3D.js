827266e395e9adca34f6c64e74c0c8826756f154
ProSep <78089273+ProSep@users.noreply.github.com>
2024-02-06 03:12:22 +0100

    Changes

--- src/components/Pages/3D.js
+++ src/components/Pages/3D.js
@@ -6,6 +6,7 @@ import Video from "../Video"
 import Oblig1 from '../../Assets/3DA/Oblig1.mp4'
 import Oblig2 from '../../Assets/3DA/Oblig2.mp4'
 import Oblig3 from '../../Assets/3DA/Oblig3.mp4'
+import Vr from '../../Assets/VR/VR.mp4'
 import Bachelor from '../../Assets/3DA/film_olje.mp4'
 import styled from "styled-components";
 import TreDBilder from "../../Assets/Images/3DBackground.png";
@@ -37,7 +38,7 @@ const TreD = () => {
             <ContentBox>
             <TextWrapper>
             <h2>Prosjekt</h2>
-                <p>Under her kan du se rendrene fra prosjektene som jeg har gjort. Det er både animasjoner og still bilder alt er modelert og animert av meg. Untaket er den siste filmen som ble gjort i gruppe da den er fra batchleren min.</p>
+                <p>Nedenfor kan du se resultatene fra prosjektene jeg har arbeidet med. Samlingen inkluderer både animasjoner og stillbilder, alle modellert og animert av meg i Autodesk Maya. I tillegg er det en skjermopptaksvideo av et VR-spill/museum som jeg og en gruppe utviklet. Det er også en animasjon fra bachelorgraden min som viser en båt som samler opp oljesøl fra havet. Denne animasjonen ble også produsert i gruppe, så ikke alt arbeidet er utført av meg alene.</p>
             </TextWrapper>
                 <Carousel data={CarouselDataGuitar} header="Guitar"/>
                 <Carousel data={CarouselDataRoom} header="Bedroom"/>
@@ -45,6 +46,7 @@ const TreD = () => {
                 <Video data={Oblig1} header='Space robot'/>
                 <Video data={Oblig2} header="Mad scientist lab"/>
                 <Video data={Oblig3} header="Lightsaber showcase"/>
+                <Video data={Vr} header="VR museum"/>
                 <Video data={Bachelor} header="Bachelor"/>
             </ContentBox>
         </>
